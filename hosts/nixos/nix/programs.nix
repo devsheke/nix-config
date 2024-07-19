6d1ab2bc583d2b4f6e27489ef755cca6b9230124
devsheke <abhi@sheke.tech>
2024-07-19 14:23:28 +0530

    Update variable name gui -> linux

--- hosts/nixos/nix/programs.nix
+++ hosts/nixos/nix/programs.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: let
   packages = import ../../../modules/nix/programs.nix pkgs;
 in {
-  environment.systemPackages = packages.tools ++ packages.devTools ++ packages.lsp ++ packages.guis;
+  environment.systemPackages = packages.tools ++ packages.devTools ++ packages.lsp ++ packages.linux;
 
   environment.variables = {
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
