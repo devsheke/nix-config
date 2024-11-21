7b8113458585a82c83781000844e6608b80f4ad5
devsheke <abhi@sheke.tech>
2024-11-22 00:48:42 +0530

    fix: update tmux config and force zsh

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,9 +1,13 @@
 {pkgs, ...}: {
   programs.tmux = with pkgs; {
     enable = true;
+    sensibleOnTop = false;
+    baseIndex = 1;
     clock24 = true;
+    disableConfirmationPrompt = true;
     historyLimit = 50000;
     mouse = true;
+    shell = "${pkgs.zsh}/bin/zsh";
     terminal = "screen-256color";
     plugins = with pkgs; [
       {plugin = tmuxPlugins.sensible;}
@@ -13,7 +17,7 @@
       {
         plugin = tmuxPlugins.rose-pine;
         extraConfig = ''
-          set -g @rose_pine_variant 'dawn'
+          set -g @rose_pine_variant 'main'
 
           set -g @rose_pine_directory 'on'
           set -g @rose_pine_date_time '%d/%m %H:%M'
@@ -31,6 +35,9 @@
           set -g @rose_pine_folder_icon ''
           set -g @rose_pine_date_time_icon ''
           set -g @rose_pine_window_status_separator " | "
+
+          set -gu default-command
+          set -g default-shell "${pkgs.zsh}/bin/zsh"
         '';
       }
     ];
