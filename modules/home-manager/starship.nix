4d8f0bddef80ecb8ad77c7e218df72901661f1a7
devsheke <abhi@sheke.tech>
2024-11-23 01:46:21 +0530

    misc: update var names

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -3,18 +3,18 @@
   pkgs,
   ...
 }: let
-  format_main = "[▒▓](#56949f)[ ${osIcon} ](bg:#56949f fg:#f2e9e1)[](fg:#56949f)$sudo$localip$directory";
-  format_git = "$git_commit$git_state$git_metrics$git_status$git_branch";
-  format_end = "$line_break$character";
-  format_right_langs = "$nix_shell$package$golang$nodejs$rust$c$lua$c$elixir$ocaml$python$zig";
+  formatMain = "[▒▓](#56949f)[ ${osIcon} ](bg:#56949f fg:#f2e9e1)[](fg:#56949f)$sudo$localip$directory";
+  formatGit = "$git_commit$git_state$git_metrics$git_status$git_branch";
+  formatEnd = "$line_break$character";
+  formatRightLangs = "$nix_shell$package$golang$nodejs$rust$c$lua$c$elixir$ocaml$python$zig";
   format_right_end = "$cmake$meson$status$battery$jobs$cmd_duration";
 in {
   programs.starship = with pkgs; {
     enable = true;
     package = starship;
     settings = {
-      format = format_main + format_git + "$docker_context" + format_end;
-      right_format = "${format_right_langs}${format_right_end}";
+      format = formatMain + formatGit + "$docker_context" + formatEnd;
+      right_format = "${formatRightLangs}${format_right_end}";
       hostname = {
         ssh_only = false;
         format = "(white) [$hostname](cyan)";
