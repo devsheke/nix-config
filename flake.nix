aca3c8c22f2cbf903afbd24b20c4b625e749c890
devsheke <abhi@sheke.tech>
2025-01-10 13:10:02 +0530

    add dwm config and move devshell config to dwm folder

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/8809585e6937d0b07fc066792c8c9abf9c3fe5c4";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
-    flake-utils.url = "github:numtide/flake-utils";
-
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,40 +20,33 @@
     self,
     nixpkgs,
     nixpkgs-stable,
-    flake-utils,
     nix-darwin,
     home-manager,
-  } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: let
-      inherit (self) outputs;
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      overlays = import ./overlays {inherit inputs;};
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    overlays = import ./overlays {inherit inputs;};
 
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [clang-tools] ++ (with xorg; [libX11 libXinerama libXft]);
-      };
+    darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
+      specialArgs = {inherit self inputs outputs;};
+      modules = [./machines/macos/nix];
+    };
 
-      darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
-        specialArgs = {inherit self inputs outputs;};
-        modules = [./machines/macos/nix];
-      };
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs outputs;};
+      modules = [./machines/nixos/nix];
+    };
 
-      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./machines/nixos/nix];
-      };
+    homeConfigurations."sheke@macos" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      extraSpecialArgs = {inherit inputs outputs;};
+      modules = [./machines/macos/home-manager];
+    };
 
-      homeConfigurations."sheke@macos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./machines/macos/home-manager];
-      };
-
-      homeConfigurations."sheke@nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./machines/nixos/home-manager];
-      };
-    });
+    homeConfigurations."sheke@nixos" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = {inherit inputs outputs;};
+      modules = [./machines/nixos/home-manager];
+    };
+  };
 }
---
+++ machines/nixos/nix/dwm/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = with pkgs; [clang-tools] ++ (with xorg; [libX11 libXinerama libXft]);
+        };
+      });
+}
