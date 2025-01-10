60f70f63fd51e6373a951a83bbe31ab8538f7023
devsheke <abhi@sheke.tech>
2025-01-10 21:57:58 +0530

    feat: add status2d and systray patches

--- machines/nixos/nix/dwm/config.def.h
+++ machines/nixos/nix/dwm/config.def.h
@@ -27,7 +27,18 @@ static const unsigned int gappov = 12;
 // 1 means no outer gap when there is only one window
 static int smartgaps = 0;
 
-#include "vanitygaps.c"
+// systray config
+// 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+// X.
+static const unsigned int systraypinning = 0;
+// 0: systray in the right corner, >0: systray on left of status text.
+static const unsigned int systrayonleft = 0;
+// systray spacing.
+static const unsigned int systrayspacing = 2;
+// 1: if pinning fails, display systray on the first monitor, False:
+// display systray on the last monitor.
+static const int systraypinningfailfirst = 1;
+static const int showsystray = 1; /* 0 means no systray */
 
 // tag underline config
 // horizontal padding between the underline and tag.
@@ -83,6 +94,8 @@ static const Rule rules[] = {
 /* ========================================================= */
 
 /* ------------------------------- Layout(s) ----------------------------- */
+
+#include "vanitygaps.c"
 
 // factor of master area size [0.05..0.95].
 static const float mfact = 0.55;
